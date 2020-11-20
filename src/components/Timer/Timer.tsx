7c76394983f8e8966b4af6ebf0c2b4847afb2531
Nouman Javed <noumanmalik960@gmail.com>
2020-11-20 21:19:45 +0500

    updated

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Timer.css";
 
-const Timer = () => {
-  return <div>Timer component</div>;
-};
+class Timer extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      minutes: 25,
+      seconds: 0,
+      isOn: false,
+    };
+  }
+
+  startTimer() {
+    console.log("Starting timer");
+  }
+
+  stopTimer() {
+    console.log("Stopping timer.");
+  }
+
+  resetTimer() {
+    console.log("Resetting timer");
+  }
+
+  render = () => {
+    return <div className="timer-container" />;
+  };
+}
 
 export default Timer;
---
+++ temp.js
@@ -0,0 +1,6 @@
+let score = '45'
+console.log(typeof (score));
+
+
+score = +score
+console.log(typeof (score));
